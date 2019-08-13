d3a692b406fb50e3136f96981c70581ebb222506
Folajimi Adekoya <fadekoya@zendesk.com>
2019-08-14 00:45:36 +0200

    exercises on variables

---
+++ variables/age.rb
@@ -0,0 +1,15 @@
+puts 'How old are you?'
+age=gets.chomp.to_i
+ten_years = 10 + age.to_i
+twenty = 20 + age
+thirty = 30 + age
+forty  = 40 + age
+puts "In 10 years you will be:"
+puts ten_years
+puts "In 20 years you will be:"
+puts twenty
+puts "In 30 years you will be:"
+puts thirty
+puts "In 40 years you will be:"
+puts forty
+#puts "In 10 years you will be " + ten_years.to_s
---
+++ variables/name.rb
@@ -0,0 +1,5 @@
+puts "Please enter your Firstname"
+firstname = gets.chomp
+puts "Please enter your Lastname"
+lastname = gets.chomp
+10.times {puts "you are welcome #{firstname} #{lastname} "}
