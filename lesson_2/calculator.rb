ac476587258bf0f30fb3c96f33ef061539879b3e
Folajimi Adekoya <fadekoya@zendesk.com>
2019-09-10 02:38:28 +0200

    calculator application

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,99 @@
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(opp)
+  case opp
+  when '1'
+    'adding'
+  when '2'
+    'subtracting'
+  when '3'
+    'multiplying'
+  when '4'
+    'dividing'
+  end
+end
+
+first_number = ''
+prompt("Welcome to the calculator app. Please enter your name:")
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt("Please use a valid name")
+  else
+    break
+  end
+end
+puts('-' * 40)
+puts("Hi #{name}")
+loop do
+  loop do
+    prompt("Please enter the first number")
+    first_number = gets.chomp
+
+    if valid_number?(first_number)
+      break
+    else
+      prompt("That does not look like a valid number")
+    end
+  end
+
+  second_number = ''
+  loop do
+    prompt("Please enter the second number")
+    second_number = gets.chomp
+
+    if valid_number?(second_number)
+      break
+    else
+      prompt("That does not look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+  Please enter the operation you will like to perform
+
+  1) to add
+  2) to subtract
+  3) to multiply
+  4) to divide
+  MSG
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("must choose 1,2,3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  case operator
+  when '1'
+    answer = first_number.to_i + second_number.to_i
+  when '2'
+    answer = first_number.to_i - second_number.to_i
+  when '3'
+    answer = first_number.to_i * second_number.to_i
+  when '4'
+    answer = first_number.to_f / second_number.to_i
+  end
+
+  prompt("The result is #{answer}")
+
+  prompt("Do you want to perform another calculation?
+    Enter Y to calculate again")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+prompt("Thank you for using my calculator. Good bye")
