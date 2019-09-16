7213bba7101a87e23c0af4d3beda399f1f07082c
Folajimi Adekoya <fadekoya@zendesk.com>
2019-09-17 00:53:18 +0200

    fix rock,paper, scisssors rubocop errors

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,34 +1,25 @@
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
-def test_method
-  prompt('test message')
-end
-
-
-
 def prompt(message)
   puts "=>#{message}"
 end
 
-test_method
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "scissors" && second == "paper") ||
+    (first == "paper" && second == "rock")
+end
 
 def display_results(player, computer)
   puts("You chose: #{player}; Computer chose: #{computer}")
-  if (player == "rock" && computer =="scissors") ||
-    (player == "scissors" && computer =="paper") ||
-    (player == "paper" && computer =="rock")
+  if win?(player, computer)
     prompt("you won")
-  elsif (player == "scissors" && computer =="rock") ||
-    (player == "paper" && computer =="scissors") ||
-    (player == "rock" && computer =="paper")
+  elsif win?(computer, player)
     prompt("Computer won")
   else
     prompt("It's a tie")
   end
 end
-
-
-
 
 loop do
   choice = ''
@@ -42,15 +33,12 @@ loop do
     end
   end
 
-
   computer_choice = VALID_CHOICES.sample
   display_results(choice, computer_choice)
-
 
   prompt("Do you want to play again? Enter y")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
-
 
 prompt("Thank you for playing, Goodbye!")
