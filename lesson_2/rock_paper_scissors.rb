6a53148bc9e9d147aaf300b585303b623c30159c
Folajimi Adekoya <fadekoya@zendesk.com>
2019-09-17 02:21:39 +0200

    added spock , lizard modification

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,4 +1,22 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+=begin
+Scissors cut Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitate Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+Rock crushes Scissors
+
+=end
+
+
+
+
+
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
 def prompt(message)
   puts "=>#{message}"
@@ -7,7 +25,15 @@ end
 def win?(first, second)
   (first == "rock" && second == "scissors") ||
     (first == "scissors" && second == "paper") ||
-    (first == "paper" && second == "rock")
+    (first == "paper" && second == "rock") ||
+    (first == "rock" && second == "lizard") ||
+    (first == "lizard" && second == "spock") ||
+    (first == "spock" && second == "scissors") ||
+    (first == "scissors" && second == "lizard") ||
+    (first == "lizard" && second == "paper") ||
+    (first == "paper" && second == "spock") ||
+    (first == "spock" && second == "rock")
+
 end
 
 def display_results(player, computer)
