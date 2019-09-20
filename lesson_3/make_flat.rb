7aa313ef7ebc2b4691b35711af51ae7e75474f85
Folajimi Adekoya <fadekoya@zendesk.com>
2019-09-21 01:14:49 +0200

    exercises from lesson3

---
+++ lesson_3/make_flat.rb
@@ -0,0 +1,12 @@
+names = ["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
+
+names.flatten!
+p names
+
+
+
+
+
+
+
+#flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
---
+++ lesson_3/string_start.rb
@@ -0,0 +1,14 @@
+
+famous_words = "seven years ago..."
+famous_words.insert(0, 'Four score ')
+
+#puts 'Four score ' + famous_words
+
+#famous_words.prepend('Four score ')
+
+#puts 'Four score ' << famous_words
+
+
+
+
+puts famous_words
