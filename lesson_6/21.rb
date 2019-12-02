06b29d0a5c15311aca523f283439264cb7457e25
Folajimi Adekoya <fadekoya@zendesk.com>
2019-12-02 20:52:52 +0100

    cleanup with rubocop

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -1,7 +1,6 @@
-
 def initialize_cards
   deck = []
-  ['Heart', 'Diamond','Clubs', 'Spades'].each do |shape|
+  ['Heart', 'Diamond', 'Clubs', 'Spades'].each do |shape|
     [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'].each do |num|
       deck << Array.new([shape, num])
     end
@@ -21,17 +20,19 @@ def hit(cards)
 end
 
 def total(cards)
-  values = cards.map {|card| card[1]}
-
+  values = cards.map { |card| card[1] }
   sum = 0
   values.each do |value|
-    if value == 'A'
-      sum += 11
-    elsif value.to_i == 0
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0
+             10
+           else
+             value.to_i
+           end
+  end
+  values.select { |value| value == "A" }.count.times do
+    sum -= 10 if sum > 21
   end
   sum
 end
@@ -40,52 +41,13 @@ def busted?(cards)
   total(cards) > 21
 end
 
-
 def prompt(text)
   puts "=> #{text}"
 end
 
-# def player_turn(cards)
-#   loop do
-#     prompt "Will you hit or stay ?"
-#     prompt "Type H to Hit and S to stay"
-#     player_choice = gets.chomp().upcase
-#     break if player_choice == 'S' || busted?(cards)
-#     my_cards = hit(cards)
-#     prompt my_cards
-#   end
-#
-#   if busted?(cards)
-#     prompt "its over hova" #end game or star agian
-#   else
-#     puts "You chose to stay!"s
-#   end
-#   my_cards
-# end
-
-# Dealer turn: when the player stays, it's the dealer's turn. The dealer must
-# follow a strict rule for determining whether to hit or stay: hit until the total is at least 17.
-#  If the dealer busts, then the player wins.
-#
-#    hit until the total is at least 17. If the dealer busts, then the player wins.
-
-# def dealer_turn(cards)
-#   loop do
-#     break if player_choice == 'S' || busted?(cards)
-#     break if total(cards) >= 17
-#     deal_cards = hit(cards)
-#   end
-#   if busted?(cards)
-#     prompt "Dealer lost it" #end game or start again
-#   else
-#     puts "Deal chose to stay!"
-#   end
-#   deal_cards
-# end
-
 def display_cards(player, dealer)
-  prompt player
-  prompt dealer
+  prompt "player cards are #{player}"
+  prompt "Dealer cards are #{dealer}"
 end
 
 def winner(player, dealer)
@@ -94,64 +56,71 @@ def winner(player, dealer)
   elsif total(player) < total(dealer)
     prompt "dealer won"
   else
-    "its a draw"
+    prompt "its a draw"
   end
 end
 
-
-
-
-#def display_cards
-
-
-
-#p deal_player_cards
-player_choice = nil
-my_cards = []
-player_cards = deal_cards
-dealer_cards = deal_cards
-dealer_card = dealer_cards.sample(1)
-
-
-prompt " you have the cards #{player_cards[0][1]} of #{player_cards[0][0]} and #{player_cards[1][1]} of #{player_cards[1][0]} "
-prompt "The dealer has the card #{dealer_card[0][1]} of #{dealer_card[0][0]} and another card"
-#
-# player_turn
-#player_turn(player_cards)
-loop do
-  prompt "Will you hit or stay ?"
-  prompt "Type H to Hit and S to stay"
-  player_choice = gets.chomp().upcase
-  break if player_choice == 'S' || busted?(player_cards)
-  player_cards = hit(player_cards)
-  prompt player_cards
-  break if busted?(player_cards)
+def play_again?
+  prompt "*******************************"
+  prompt "*******************************"
+  prompt "Do you want to play again? (y or n)"
+  answer = gets.chomp
+  answer.downcase.start_with?('y')
 end
-
-if busted?(player_cards)
-  prompt "you lost because you are busted"  #end game or star agian
-  exit(0)
-else
-  puts "You chose to stay!"
-end
-
-
-
-#dealer_turn(dealer_cards)
-# player_turn
 
 loop do
-  break if total(dealer_cards) >= 17 || busted?(dealer_cards)
-  deal_cards = hit(dealer_cards)
-  prompt dealer_cards
-end
-if busted?(dealer_cards)
-  prompt "Dealer got busted" #end game or start again
-  exit(0)
-else
-  puts "Deal chose to stay!"
-end
+  player_choice = nil
+  player_cards = deal_cards
+  dealer_cards = deal_cards
+  dealer_card = dealer_cards.sample(1)
 
+  prompt " you have the cards #{player_cards[0][1]} of #{player_cards[0][0]}
+  and #{player_cards[1][1]} of #{player_cards[1][0]} "
+  prompt "The dealer has the card #{dealer_card[0][1]} of #{dealer_card[0][0]}
+  and another card"
 
-display_cards(player_cards, dealer_cards)
-winner(player_cards, dealer_cards)
+  # player's turn
+  loop do
+    prompt "Will you hit or stay ?"
+    prompt "Type H to Hit and S to stay"
+    player_choice = gets.chomp.upcase
+    if player_choice == 'S' || busted?(player_cards)
+      break
+    elsif player_choice == 'H'
+      player_cards = hit(player_cards)
+      prompt " You chose to Hit"
+      prompt player_cards
+      break if busted?(player_cards)
+    else
+      prompt "Sorry, you must enter 'H' or 'S'."
+      prompt player_cards
+    end
+  end
+
+  if busted?(player_cards)
+    prompt "you lost because you are busted"
+    display_cards(player_cards, dealer_cards)
+    play_again? ? next : break
+  else
+    puts "You chose to stay!"
+  end
+  # Dealer's turn
+  loop do
+    break if  total(dealer_cards) >= 17 || busted?(dealer_cards)
+
+    dealer_cards = hit(dealer_cards)
+    prompt dealer_cards
+  end
+  if busted?(dealer_cards)
+    prompt "Dealer got busted"
+    display_cards(player_cards, dealer_cards)
+    play_again? ? next : break
+  else
+    puts "Deal chose to stay!"
+  end
+
+  display_cards(player_cards, dealer_cards)
+  winner(player_cards, dealer_cards)
+
+  break unless play_again?
+end
